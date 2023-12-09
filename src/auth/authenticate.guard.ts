65dddbbff1e92aad0c47b4ddcea2e475355fb074
werlic <agussulaeman453@yahoo.co.id>
2023-12-09 07:16:18 +0700

    change auth to jwt

--- src/auth/authenticate.guard.ts
+++ src/auth/authenticate.guard.ts
@@ -1,17 +1,55 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+// @Injectable()
+// export class AuthenticateGuard implements CanActivate {
+//   canActivate(
+//     context: ExecutionContext,
+//   ): boolean | Promise<boolean> | Observable<boolean> {
+//     const request = context.switchToHttp().getRequest();
+//     const response = context.switchToHttp().getResponse();
+//     if (request.isAuthenticated()) {
+//       return true;
+//     }
+    
+//     return response.redirect('/login');
+//   }
+// }
 
 @Injectable()
 export class AuthenticateGuard implements CanActivate {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  constructor(private readonly jwtService: JwtService) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const response = context.switchToHttp().getResponse();
-    if (request.isAuthenticated()) {
-      return true;
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
     }
-    
-    return response.redirect('/login');
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        {
+          secret: process.env.JWT_SECRET
+        }
+      );
+      // 💡 We're assigning the payload to the request object here
+      // so that we can access it in our route handlers
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    if (request.headers.authorization) {
+      const [type, token] = request.headers.authorization.split(' ') ?? [];
+      return type.trim().toLowerCase() === 'bearer' ? token?.trim() : undefined;
+    } else if (request.signedCookies) {
+      const token = request.signedCookies['client_token'] ?? undefined;
+      return token;
+    }
+    return undefined;
   }
 }
